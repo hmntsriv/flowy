2c423a9074015bde5a5c0a8d7e355042b67ec7fb
Hemant Srivastava <166055665+hmntsriv@users.noreply.github.com>
2026-09-14 07:39:58 +0530

    style: load editor and mobile fixes

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -5,6 +5,7 @@ import "./styles/variables.css";
 import "./styles/globals.css";
 import "./styles/layout.css";
 import "./styles/components.css";
+import "./styles/production-fixes.css";
 
 import { AuthProvider } from "./context/AuthContext";
 
